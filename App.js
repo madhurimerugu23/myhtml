3db12c8d3331aa8e33571561f1e0cf703d06ef55
madhurimerugu23 <152799670+madhurimerugu23@users.noreply.github.com>
2024-10-16 15:38:51 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,52 +1,31 @@
-import React from "react";
-import * as ReactDOM from 'react-dom';
-import logo from './globe.jpg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class HelloWorld extends React.Component {
 
-function App() {
+  render() {
+  
+  const continents = ['Africa','America','Asia','Australia','Europe'];
+  
+  const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+  
+  const message = helloContinents.join(' ');
+  
   return (
-    <div className="App">
-     
-      <header className="App-header">
-      <h3>M.Madhuri</h3>
-      <h2>23255A0505</h2>
-      <br></br>
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-
-          <br></br>
-         know more about globe
-        </p>
-        <a
-          className="App-link"
-          href="https://en.wikipedia.org/wiki/Globe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn 
-        </a>
-      </header>
-    </div>
+  
+  <div title="Outer div">
+  
+  <h1>{message}</h1>
+  
+  </div>
+  
   );
-}
-
-
-export default App;
-/*class HelloWord extends React.Component
-{
-  render()
-  {
-  const continents=['asia','america','africa','srilanka'];
-  const helloc=Array.from(continents,c=> 'hello ${c}!');
-    const msg=helloc.join(' ');
-    return(
-    <div title="Ouetr div">
-      <h3>{message}</h3>
-    </div>
-    );
+  
   }
+  
+  }
+  export default HelloWorld
+  const element = <HelloWorld />;
 
-}
-const obj=<HelloWord/>;
-ReactDOM.render(obj,document.getElementById(contents));*/
+  
+   
